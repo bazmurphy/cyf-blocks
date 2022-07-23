0744c647a4e38414dfb26bec8e7a963bde6d71b9
Baz Murphy <bazmurphy@gmail.com>
2022-07-23 06:32:06 +0100

    renamed from ex 24 to ex 23, and added new exercises

---
+++ testing/exercise2301/01/main.js
@@ -0,0 +1,71 @@
+document.getElementById('add-banana').addEventListener('click', (event) => {
+    let element_shopping_cart = document.getElementById('shopping-cart');
+    let new_li = document.createElement('li');
+    new_li.innerText = 'Banana🍌';
+
+    element_shopping_cart.appendChild(new_li);
+
+});
+
+document.getElementById('add-mango').addEventListener('click', (event) => {
+    let element_shopping_cart2 = document.getElementById('shopping-cart');
+    let new_li2 = document.createElement('li');
+    new_li2.innerText = 'Mango🥭';
+
+    element_shopping_cart2.appendChild(new_li2);
+
+});
+
+document.getElementById('add-pineapple').addEventListener('click', (event) => {
+    let element_shopping_cart3 = document.getElementById('shopping-cart');
+    let new_li3 = document.createElement('li');
+    new_li3.innerText = 'Pineapple🍍';
+
+    element_shopping_cart3.appendChild(new_li3);
+
+});
+
+document.getElementById('add-kiwi').addEventListener('click', (event) => {
+    let element_shopping_cart4 = document.getElementById('shopping-cart');
+    let new_li4 = document.createElement('li');
+    new_li4.innerText = 'Kiwi🥝';
+
+    element_shopping_cart4.appendChild(new_li4);
+
+});
+
+document.getElementById('add-watermelon').addEventListener('click', (event) => {
+    let element_shopping_cart5 = document.getElementById('shopping-cart');
+    let new_li5 = document.createElement('li');
+    new_li5.innerText = 'Watermelon🍉';
+
+    element_shopping_cart5.appendChild(new_li5);
+
+});
+
+document.getElementById('add-peach').addEventListener('click', (event) => {
+    let element_shopping_cart6 = document.getElementById('shopping-cart');
+    let new_li6 = document.createElement('li');
+    new_li6.innerText = 'Peach🍑';
+
+    element_shopping_cart6.appendChild(new_li6);
+
+});
+
+document.getElementById('add-strawberries').addEventListener('click', (event) => {
+    let element_shopping_cart7 = document.getElementById('shopping-cart');
+    let new_li7 = document.createElement('li');
+    new_li7.innerText = 'Stawberries🍓';
+
+    element_shopping_cart7.appendChild(new_li7);
+
+});
+
+document.getElementById('add-apple').addEventListener('click', (event) => {
+    let element_shopping_cart8 = document.getElementById('shopping-cart');
+    let new_li8 = document.createElement('li');
+    new_li8.innerText = 'Apple🍏';
+
+    element_shopping_cart8.appendChild(new_li8);
+
+});
